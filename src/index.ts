39a71334aa7d360c5c4345272e01b3b8f990952c
CptTheYe <99993506+CptTheYe@users.noreply.github.com>
2022-09-15 12:04:50 +0300

    15.09.2022 Runni esimese ülesanne tehtud

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World");
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'Hello worls!',
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running');
+});
